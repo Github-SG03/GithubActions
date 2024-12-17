484e825b5da3ca7ba2a4df62112438c14e09e86b
Github-SG03 <sgs.shivam99@outlook.com>
2024-12-17 22:20:04 +0530

    python1.py

--- python1.py
+++ python1.py
@@ -1,2 +1,12 @@
-# Simple Python script to print "Hello, world!"
-print("Hello, world!")
+# Simple Python script to print "Hello, GitHub Codespaces! using Flask library"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, GitHub Codespaces!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
+
